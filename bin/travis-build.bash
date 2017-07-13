520f1de3e5e1009d781479c79efcc28e5c0dc83c
amercader <amercadero@gmail.com>
2017-07-13 21:46:01 +0100

    Use master

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -10,9 +10,10 @@ sudo apt-get install postgresql-$PGVERSION solr-jetty
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-export latest_ckan_release_branch=`git branch --all | grep remotes/origin/release-v | sort -r | sed 's/remotes\/origin\///g' | head -n 1`
-echo "CKAN branch: $latest_ckan_release_branch"
-git checkout $latest_ckan_release_branch
+if [ $CKANVERSION != 'master' ]
+then
+    git checkout release-v$CKANVERSION-latest
+fi
 python setup.py develop
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
