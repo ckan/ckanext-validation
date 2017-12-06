827b67dcc019ce6a9e9df690a48fa96f9f79172c
Samuel Okoroafor <samuel.i.okoroafor@gmail.com>
2017-12-06 17:32:56 +0100

    Fix comma error

--- ckanext/validation/jobs.py
+++ ckanext/validation/jobs.py
@@ -96,7 +96,7 @@ def run_validation_job(resource):
 
 def _validate_table(source, _format=u'csv', schema=None, **options):
 
-    report = validate(source, format=_format, schema=schema, skip_checks=['duplicate-rows'] **options)
+    report = validate(source, format=_format, schema=schema, skip_checks=['duplicate-rows'], **options)
 
     log.debug(u'Validating source: {}'.format(source))
    
