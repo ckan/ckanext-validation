5147ed12b35346a35f6ca4fcd6373f4a574d75ca
Ana <ana.trajkovska@keitaro.com>
2018-08-08 00:21:01 +0200

    Translate strings in JS file

--- ckanext/validation/fanstatic/js/module-validation-report.js
+++ ckanext/validation/fanstatic/js/module-validation-report.js
@@ -1,14 +1,32 @@
+
 this.ckan.module('validation-report', function (jQuery) {
   return {
     options: {
       report: null
     },
     initialize: function() {
-      goodtablesUI.render(
-        goodtablesUI.Report,
-        {report: this.options.report},
-        this.el[0]
-      )
+      function loadScript(url, callback)
+      {
+          var head = document.getElementsByTagName('head')[0];
+          var script = document.createElement('script');
+          script.type = 'text/javascript';
+          script.src = url;
+
+          script.onreadystatechange = callback;
+          script.onload = callback;
+
+          head.appendChild(script);
+      }
+      var scriptLoaded = function() {
+        goodtablesUI.render(
+          goodtablesUI.Report,
+          {report: this.options.report},
+          this.el[0]
+        )
+     }.bind(this);
+
+     // Dynamically load the goodtables-ui script so that strings located there can be translated
+     loadScript('/goodtables-ui.js', scriptLoaded) 
     }
   }
 });
