1f09a821317bffb91364e202d508b7075b782292
amercader <amercadero@gmail.com>
2021-09-17 10:59:55 +0200

    Remove asserts

--- ckanext/validation/tests/test_utils.py
+++ ckanext/validation/tests/test_utils.py
@@ -3,90 +3,89 @@ import uuid
 import mock
 
 from pyfakefs import fake_filesystem_unittest
-from nose.tools import assert_equals
 
 from ckan.tests.helpers import change_config
 
 from ckanext.validation.tests.helpers import mock_uploads
 from ckanext.validation.utils import (
-    get_create_mode_from_config, get_update_mode_from_config,
-    get_local_upload_path, delete_local_uploaded_file
+    get_create_mode_from_config,
+    get_update_mode_from_config,
+    get_local_upload_path,
+    delete_local_uploaded_file,
 )
 
 
 class TestConfig(object):
-
     def test_config_defaults(self):
 
-        assert_equals(get_update_mode_from_config(), 'async')
-        assert_equals(get_create_mode_from_config(), 'async')
+        assert get_update_mode_from_config() == "async"
+        assert get_create_mode_from_config() == "async"
 
-    @change_config('ckanext.validation.run_on_update_sync', True)
+    @change_config("ckanext.validation.run_on_update_sync", True)
     def test_config_update_true_sync(self):
 
-        assert_equals(get_update_mode_from_config(), 'sync')
+        assert get_update_mode_from_config() == "sync"
 
-    @change_config('ckanext.validation.run_on_update_sync', False)
+    @change_config("ckanext.validation.run_on_update_sync", False)
     def test_config_update_false_sync(self):
 
-        assert_equals(get_update_mode_from_config(), 'async')
+        assert get_update_mode_from_config() == "async"
 
-    @change_config('ckanext.validation.run_on_create_sync', True)
+    @change_config("ckanext.validation.run_on_create_sync", True)
     def test_config_create_true_sync(self):
 
-        assert_equals(get_create_mode_from_config(), 'sync')
+        assert get_create_mode_from_config() == "sync"
 
-    @change_config('ckanext.validation.run_on_create_sync', False)
+    @change_config("ckanext.validation.run_on_create_sync", False)
     def test_config_create_false_sync(self):
 
-        assert_equals(get_create_mode_from_config(), 'async')
+        assert get_create_mode_from_config() == "async"
 
-    @change_config('ckanext.validation.run_on_update_async', True)
+    @change_config("ckanext.validation.run_on_update_async", True)
     def test_config_update_true_async(self):
 
-        assert_equals(get_update_mode_from_config(), 'async')
+        assert get_update_mode_from_config() == "async"
 
-    @change_config('ckanext.validation.run_on_update_async', False)
+    @change_config("ckanext.validation.run_on_update_async", False)
     def test_config_update_false_async(self):
 
-        assert_equals(get_update_mode_from_config(), None)
+        assert get_update_mode_from_config() is None
 
-    @change_config('ckanext.validation.run_on_create_async', True)
+    @change_config("ckanext.validation.run_on_create_async", True)
     def test_config_create_true_async(self):
 
-        assert_equals(get_create_mode_from_config(), 'async')
+        assert get_create_mode_from_config() == "async"
 
-    @change_config('ckanext.validation.run_on_create_async', False)
+    @change_config("ckanext.validation.run_on_create_async", False)
     def test_config_create_false_async(self):
 
-        assert_equals(get_create_mode_from_config(), None)
+        assert get_create_mode_from_config() is None
 
-    @change_config('ckanext.validation.run_on_update_async', False)
-    @change_config('ckanext.validation.run_on_create_async', False)
+    @change_config("ckanext.validation.run_on_update_async", False)
+    @change_config("ckanext.validation.run_on_create_async", False)
     def test_config_both_false(self):
 
-        assert_equals(get_update_mode_from_config(), None)
-        assert_equals(get_create_mode_from_config(), None)
+        assert get_update_mode_from_config() is None
+        assert get_create_mode_from_config() is None
 
 
 class TestFiles(object):
-
     @mock_uploads
     def test_local_path(self, mock_open):
 
         resource_id = str(uuid.uuid4())
 
-        assert_equals(
-            get_local_upload_path(resource_id),
-            '/doesnt_exist/resources/{}/{}/{}'.format(
-                resource_id[0:3], resource_id[3:6], resource_id[6:])
+        assert get_local_upload_path(
+            resource_id
+        ) == "/doesnt_exist/resources/{}/{}/{}".format(
+            resource_id[0:3], resource_id[3:6], resource_id[6:]
         )
 
     @mock_uploads
     def test_delete_upload_file(self, mock_open):
 
         resource_id = str(uuid.uuid4())
-        path = '/doesnt_exist/resources/{}/{}/{}'.format(
+        path = "/doesnt_exist/resources/{}/{}/{}".format(
             resource_id[0:3], resource_id[3:6], resource_id[6:]
         )
 
@@ -106,19 +105,20 @@ class TestFiles(object):
     def test_delete_file_not_deleted_if_resources_first(self, mock_open):
 
         resource_id = str(uuid.uuid4())
-        path = '/doesnt_exist/resources/{}'.format(resource_id)
+        path = "/doesnt_exist/resources/{}".format(resource_id)
 
         patcher = fake_filesystem_unittest.Patcher()
         patcher.setUp()
         patcher.fs.CreateFile(path)
 
         assert os.path.exists(path)
-        with mock.patch('ckanext.validation.utils.get_local_upload_path',
-                        return_value=path):
+        with mock.patch(
+            "ckanext.validation.utils.get_local_upload_path", return_value=path
+        ):
             delete_local_uploaded_file(resource_id)
 
         assert not os.path.exists(path)
-        assert os.path.exists('/doesnt_exist/resources')
+        assert os.path.exists("/doesnt_exist/resources")
 
         patcher.tearDown()
 
@@ -126,19 +126,20 @@ class TestFiles(object):
     def test_delete_file_not_deleted_if_resources_second(self, mock_open):
 
         resource_id = str(uuid.uuid4())
-        path = '/doesnt_exist/resources/data/{}'.format(resource_id)
+        path = "/doesnt_exist/resources/data/{}".format(resource_id)
 
         patcher = fake_filesystem_unittest.Patcher()
         patcher.setUp()
         patcher.fs.CreateFile(path)
 
         assert os.path.exists(path)
-        with mock.patch('ckanext.validation.utils.get_local_upload_path',
-                        return_value=path):
+        with mock.patch(
+            "ckanext.validation.utils.get_local_upload_path", return_value=path
+        ):
             delete_local_uploaded_file(resource_id)
 
         assert not os.path.exists(path)
-        assert os.path.exists('/doesnt_exist/resources')
+        assert os.path.exists("/doesnt_exist/resources")
 
         patcher.tearDown()
 
@@ -146,7 +147,7 @@ class TestFiles(object):
     def test_delete_passes_if_os_exeception(self, mock_open):
 
         resource_id = str(uuid.uuid4())
-        path = '/doesnt_exist/resources/{}/{}/{}'.format(
+        path = "/doesnt_exist/resources/{}/{}/{}".format(
             resource_id[0:3], resource_id[3:6], resource_id[6:]
         )
 
@@ -155,10 +156,8 @@ class TestFiles(object):
         patcher.fs.CreateFile(path)
 
         assert os.path.exists(path)
-        with mock.patch('ckanext.validation.utils.os.remove',
-                        side_effect=OSError):
+        with mock.patch("ckanext.validation.utils.os.remove", side_effect=OSError):
 
             delete_local_uploaded_file(resource_id)
-
 
         patcher.tearDown()
--- ckanext/validation/tests/test_validators.py
+++ ckanext/validation/tests/test_validators.py
@@ -1,66 +1,68 @@
 import json
-
-from nose.tools import assert_raises, assert_equals
+import pytest
 
 from ckantoolkit import Invalid
 
-from ckan.tests.helpers import change_config
-
 from ckanext.validation.validators import (
-    resource_schema_validator, validation_options_validator
+    resource_schema_validator,
+    validation_options_validator,
 )
 
 
 class TestResourceSchemaValidator(object):
-
     def test_resource_schema_none(self):
 
-        schema = ''
+        schema = ""
 
         assert resource_schema_validator(schema, {}) is None
 
     def test_resource_schema_invalid_json_string(self):
 
-        schema = '{a,b}'
+        schema = "{a,b}"
 
-        assert_raises(Invalid, resource_schema_validator, schema, {})
+        with pytest.raises(Invalid):
+            resource_schema_validator(schema, {})
 
     def test_resource_schema_invalid_schema_string(self):
 
         schema = '{"a": 1}'
 
-        assert_raises(Invalid, resource_schema_validator, schema, {})
+        with pytest.raises(Invalid):
+            resource_schema_validator(schema, {})
 
     def test_resource_schema_valid_json_not_a_dict_string(self):
 
-        schema = '[a,2]'
+        schema = "[a,2]"
 
-        assert_raises(Invalid, resource_schema_validator, schema, {})
+        with pytest.raises(Invalid):
+            resource_schema_validator(schema, {})
 
     def test_resource_schema_valid_json_empty_string(self):
 
         schema = '""'
 
-        assert_raises(Invalid, resource_schema_validator, schema, {})
+        with pytest.raises(Invalid):
+            resource_schema_validator(schema, {})
 
     def test_resource_schema_invalid_schema_object(self):
 
-        schema = {'a': 1}
+        schema = {"a": 1}
 
-        with assert_raises(Invalid) as e:
+        with pytest.raises(Invalid) as e:
             resource_schema_validator(schema, {})
 
-        assert e.exception.error.startswith(
-            'Invalid Table Schema: ' +
-            'Descriptor validation error: \'fields\' is a required property')
+        assert e.value.error.startswith(
+            "Invalid Table Schema: "
+            + "Descriptor validation error: 'fields' is a required property"
+        )
 
     def test_resource_schema_valid_schema_object(self):
 
-        schema = {'fields': [{'name': 'longitude'}]}
+        schema = {"fields": [{"name": "longitude"}]}
 
         value = resource_schema_validator(schema, {})
 
-        assert_equals(value, json.dumps(schema))
+        assert value == json.dumps(schema)
 
     def test_resource_schema_valid_schema_string(self):
 
@@ -68,54 +70,58 @@ class TestResourceSchemaValidator(object):
 
         value = resource_schema_validator(schema, {})
 
-        assert_equals(value, schema)
+        assert value == schema
 
     def test_resource_schema_valid_schema_url(self):
 
-        schema = 'https://example.com/schema.json'
+        schema = "https://example.com/schema.json"
 
         value = resource_schema_validator(schema, {})
 
-        assert_equals(value, schema)
+        assert value == schema
 
     def test_resource_schema_invalid_wrong_url(self):
 
-        schema = '/some/wrong/url/schema.json'
+        schema = "/some/wrong/url/schema.json"
 
-        assert_raises(Invalid, resource_schema_validator, schema, {})
+        with pytest.raises(Invalid):
+            resource_schema_validator(schema, {})
 
 
 class TestValidationOptionsValidator(object):
-    '''
+    """
     Note: At the point this validator is run the value should already
         be a valid JSON string (ie `scheming_valid_json_object` has been
         run)
-    '''
+    """
 
     def test_no_default_validation_options(self):
 
         value = '{"headers":3}'
 
-        assert_equals(validation_options_validator(value, {}), value)
+        assert validation_options_validator(value, {}) == value
 
-    @change_config('ckanext.validation.default_validation_options',
-                   '{"delimiter":";"}')
+    @pytest.mark.ckan_config(
+        "ckanext.validation.default_validation_options", '{"delimiter":";"}'
+    )
     def test_default_validation_options(self):
 
         value = '{"headers": 3}'
 
-        assert_equals(
-            validation_options_validator(value, {}),
-            '{"delimiter": ";", "headers": 3}'
+        assert (
+            validation_options_validator(value, {})
+            == '{"delimiter": ";", "headers": 3}'
         )
 
-    @change_config('ckanext.validation.default_validation_options',
-                   '{"delimiter":";", "headers":2}')
+    @pytest.mark.ckan_config(
+        "ckanext.validation.default_validation_options",
+        '{"delimiter":";", "headers":2}',
+    )
     def test_default_validation_optionsi_does_not_override(self):
 
         value = '{"headers": 3}'
 
-        assert_equals(
-            validation_options_validator(value, {}),
-            '{"delimiter": ";", "headers": 3}'
+        assert (
+            validation_options_validator(value, {})
+            == '{"delimiter": ";", "headers": 3}'
         )
