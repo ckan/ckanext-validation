79cf7d3e99960dcb903a07f212e18e9028cc7a9e
amercader <amercadero@gmail.com>
2021-09-17 12:41:03 +0200

    db init cli

--- ckanext/validation/plugin/flask_plugin.py
+++ ckanext/validation/plugin/flask_plugin.py
@@ -2,13 +2,19 @@
 
 from ckan import plugins as p
 
-from ckanext.validation import blueprints
+from ckanext.validation import blueprints, cli
 
 
 class ValidationMixin(p.SingletonPlugin):
     p.implements(p.IBlueprint)
+    p.implements(p.IClick)
 
     # IBlueprint
 
     def get_blueprint(self):
         return [blueprints.validation]
+
+    # IClick
+
+    def get_commands(self):
+        return [cli.validation]
