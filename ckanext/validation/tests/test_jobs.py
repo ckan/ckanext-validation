91ba8f565cd7ec371a1d647344c064d24549f356
Edgar Zanella Alvarenga <e@vaz.io>
2022-09-26 14:28:41 +0200

    correct schema comparing test at TestValidationJob.test_job_run_schema

--- ckanext/validation/tests/test_jobs.py
+++ ckanext/validation/tests/test_jobs.py
@@ -81,7 +81,7 @@ class TestValidationJob(object):
 
         assert mock_validate.call_args[0][0] == "http://example.com/file.csv"
         assert mock_validate.call_args[1]["format"] == "csv"
-        assert mock_validate.call_args[1]["schema"] == schema
+        assert mock_validate.call_args[1]["schema"] == json.dumps(schema)
 
     @mock.patch("ckanext.validation.jobs.validate", return_value=VALID_REPORT)
     @mock.patch.object(
