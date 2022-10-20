4d5196283e6499ab47f6eff6c3cac341210fb1da
Edgar Zanella Alvarenga <e@vaz.io>
2022-10-20 13:47:06 +0200

    add frictionless report component and use it in report page

--- ckanext/validation/webassets/js/module-validation-report.js
+++ ckanext/validation/webassets/js/module-validation-report.js
@@ -1,14 +1,13 @@
-this.ckan.module('validation-report', function (jQuery) {
+"use strict";
+
+ckan.module('validation-report', function (jQuery) {
   return {
     options: {
       report: null
     },
     initialize: function() {
-      goodtablesUI.render(
-        goodtablesUI.Report,
-        {report: this.options.report},
-        this.el[0]
-      )
+      let element = document.getElementById('report')
+      frictionlessComponents.render(frictionlessComponents.Report, { options.report }, element)
     }
   }
 });
