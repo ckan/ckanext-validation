52ca754826e9d5e7f8f06ea3109f7b64e9a9cd18
Edgar Zanella Alvarenga <e@vaz.io>
2022-12-13 21:56:36 +0100

    get variables from resource form from hidden inputs

--- ckanext/validation/webassets/js/module-ckan-uploader.js
+++ ckanext/validation/webassets/js/module-ckan-uploader.js
@@ -4,11 +4,6 @@ ckan.module('ckan-uploader', function (jQuery) {
   return {
     options: {
       upload_url: '',
-      dataset_id: '',
-      resource_id: '',
-      url_type: '',
-      current_url: false,
-      update: false
     },
     afterUpload: (resource_id) => (evt) => {
       let resource = evt.detail
@@ -38,14 +33,20 @@ ckan.module('ckan-uploader', function (jQuery) {
       }
     },
     initialize: function() {
+      let resource_id = document.getElementById('resource_id').value
+      let dataset_id = document.getElementById('dataset_id').value
+      let current_url = document.getElementById('current_url').value
+      let url_type = document.getElementById('url_type').value
+      let update = document.getElementById('update').value
+
       CkanUploader('ckan_uploader', 
                     this.options.upload_url,
-                    this.options.dataset_id,
-                    (this.options.resource_id != true)?this.options.resource_id:'',
-                    (this.options.url_type != true)?this.options.url_type:'',
-                    (this.options.current_url != true)?this.options.current_url:'',
-                    (this.options.update != true)?this.options.update:'')
-      document.getElementById('ckan_uploader').addEventListener('fileUploaded', this.afterUpload(this.options.resource_id))
+                    dataset_id,
+                    resource_id,
+                    url_type,
+                    current_url,
+                    update)
+      document.getElementById('ckan_uploader').addEventListener('fileUploaded', this.afterUpload(resource_id))
     }
   }
 });
