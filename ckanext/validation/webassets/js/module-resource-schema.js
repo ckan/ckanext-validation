56ab8bbb86a2892fa080accd9a831f90f22d19e6
Edgar Zanella Alvarenga <e@vaz.io>
2023-02-08 23:24:29 +0100

    Remove scrolling to schema json on resource edit

--- ckanext/validation/webassets/js/module-resource-schema.js
+++ ckanext/validation/webassets/js/module-resource-schema.js
@@ -59,10 +59,13 @@ ckan.module('resource-schema', function($) {
       this.label_url = $('label', this.field_url);
 
       this.field_upload_input.on('change', this._onInputChange);
-      this.field_url_input.focus()
-        .on('blur', this._onURLBlur);
-      this.field_json_input.focus()
-        .on('blur', this._onJSONBlur);
+      // With the follow lines the form is being scrolled down to the
+      // schema fields always when the user open the resource edit form
+      //
+      // this.field_url_input.focus()
+      //   .on('blur', this._onURLBlur);
+      // this.field_json_input.focus()
+      //   .on('blur', this._onJSONBlur);
 
       // Button to set upload a schema file
       this.button_upload = $('<a href="javascript:;" class="btn btn-default">' +
